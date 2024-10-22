323a6005507cdb2da3436c9ecf12d974906fba56
Gül Karatas <123191066+GulKaratas@users.noreply.github.com>
2024-10-22 23:01:09 +0300

    Initial Commit

---
+++ Weathery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Gül Karataş on 22.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
